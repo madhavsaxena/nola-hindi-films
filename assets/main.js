a11302732f954e6582f7a0964cca002bd0c3f892
Madhav Saxena <madhav@namassolutions.com>
2018-10-23 23:48:49 +0000

    dev to production url change

--- assets/main.js
+++ assets/main.js
@@ -4,8 +4,7 @@ xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var fcheck = JSON.parse(this.responseText);
         document.getElementById("answer").innerHTML = fcheck.status;
-        document.getElementById("todaysdate").innerHTML = fcheck.status;
     }
 };
-xmlhttp.open("GET", "http://167.99.145.40/hindifilms/filmstatus.json", true);
+xmlhttp.open("GET", "http://hindifilmsnola.com/filmstatus.json", true);
 xmlhttp.send();
